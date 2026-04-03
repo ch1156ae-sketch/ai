79db63b2e33d5dcb987278fd2dff64c14f32558a
ch1156ae-sketch <ch1156ae@gmail.com>
2026-04-03 09:29:44 +0900

    Add files via upload

--- 18_formData/js/script.js
+++ 18_formData/js/script.js
@@ -1,16 +1,17 @@
-    const scriptURL = '< add you own link here >' // add your own app script link here
-    const form = document.forms['submit-to-google-sheet']
-    const msg = document.getElementById("msg")
-  
-    form.addEventListener('submit', e => {
-      e.preventDefault()
-      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            msg.innerHTML = "Message sent successfully"
-            setTimeout(function(){
-                msg.innerHTML = ""
-            },5000)
-            form.reset()
-        })
-        .catch(error => console.error('Error!', error.message))
+const scriptURL =
+  "https://script.google.com/macros/s/AKfhttps://script.google.com/macros/s/AKfycbzI0M4l__zJ-_oqvOhQs4mjiu5FAFNDknzAeuIjthrvK2JTnJY_ODU583RhKHvH3LFizw/execycbxNnjzD9NqsCDdHSekBxCbbKH6a_WSRA4GVhR2seZ_GC4cxxEC-QVCV4dWP5khvM_Sy/exec"; // add your own app script link here
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
     })
+    .catch((error) => console.error("Error!", error.message));
+});
