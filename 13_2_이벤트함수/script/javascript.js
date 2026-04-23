357e8dc5bd62a1a915e03e535b83c41a831e6705
ch1156ae-sketch <ch1156ae@gmail.com>
2026-04-23 12:09:14 +0900

    Add files via upload

--- 13_2_이벤트함수/script/javascript.js
+++ 13_2_이벤트함수/script/javascript.js
@@ -4,12 +4,18 @@ console.log(btn1);
 let btn2 = document.querySelector("#btn2");
 let btn3 = document.querySelector("#btn3");
 let btn4 = document.querySelector("#btn4");
+let btn5 = document.querySelector("#btn5");
+let btn6 = document.querySelector("#btn6");
+let btn7 = document.querySelector("#btn7");
+let btn8 = document.querySelector("#btn8");
 
 //box
 let fadebox = document.querySelector("#fadebox");
 console.log(fadebox);
 let fadetogglebox = document.querySelector("#fadetogglebox");
 let upbox = document.querySelector("#up");
+let slidetgbox = document.querySelector("#slidetogglebox");
+let anibox = document.querySelector("#anim");
 
 // btn1을 클릭하면 fadebox를 부드럽게 사라지게
 btn1.addEventListener("click", function () {
@@ -30,4 +36,18 @@ btn3.addEventListener("click", function () {
 
 btn4.addEventListener("click", function () {
     upbox.classList.add("slide-hidden");
+});
+btn5.addEventListener("click", function () {
+    upbox.classList.remove("slide-hidden");
+});
+
+btn6.addEventListener("click", function () {
+    slidetgbox.classList.toggle("slide-hidden");
+});
+
+btn7.addEventListener("click", function () {
+    anibox.classList.add("ani-move");
+});
+btn8.addEventListener("click", function () {
+    anibox.classList.remove("ani-move");
 });
